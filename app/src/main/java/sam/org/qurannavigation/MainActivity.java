a1f35e7e2e3a4f6e78926e11fcb2263d1440733e
bitf19m043@pucit.edu.pk <bitf19m043@pucit.edu.pk>
2023-02-16 09:59:02 +0500

    Setting XML design a bit

--- app/src/main/java/sam/org/qurannavigation/MainActivity.java
+++ app/src/main/java/sam/org/qurannavigation/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    // creating objects for xml attributes
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
